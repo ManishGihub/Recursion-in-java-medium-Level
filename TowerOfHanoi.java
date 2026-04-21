11bc8d7a370b25b2bf3f93149df3e651a3b359ca
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-04-21 22:11:22 +0530

    Done

---
+++ TowerOfHanoi.java
@@ -0,0 +1,16 @@
+package RecursionM;
+
+public class TowerOfHanoi {
+    public static void TOH(int n,String src, String helper, String dest){
+        if(n == 1){
+            System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+            return;
+        }
+        TOH(n-1, src, dest, helper);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        TOH(n-1, helper, src, dest);
+    }
+    public static void main(String[] args) {
+        TOH(1,"S", "H", "D");
+    }
+}
